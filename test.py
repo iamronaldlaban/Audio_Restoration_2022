40c25c909e744b1861cb6e3981c8ebc7094cc533
Laban <labanr@AD.MEE.TCD.IE>
2022-11-25 19:29:38 +0000

    Final Median Filter with plots

--- test.py
+++ test.py
@@ -1,45 +1,59 @@
 import numpy as np
 import scipy
-from playsound import playsound
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 from sklearn.metrics import mean_squared_error
+from datetime import datetime
 from time import sleep
 from tqdm import tqdm
 
-
-samplerate, data = wavfile.read('degraded.wav')
-length = data.shape[0] / samplerate
-print(f"length = {length}s")
-
-# playsound('degrade.wav')
-
-time = np.linspace(0., length, data.shape[0])
-plt.figure(figsize=(15, 5))
-plt.plot(time, data, label="Degraded Signal")
+#Import the ORGINAL wave file
+samplerate_org, data_org = wavfile.read('original.wav')
+length_org = data_org.shape[0] / samplerate_org
+time_org = np.linspace(0., length_org, data_org.shape[0])
+plt.subplot(3, 1, 1)
+plt.plot(time_org,data_org)
+plt.subplots_adjust(hspace=0.5)
+plt.title("Original Signal")
 plt.xlabel("Time [s]")
-plt.ylabel("Amplitude")
-plt.show()
+plt.ylabel("Amplitude") 
+
+
+#Import the DEGRADED wave file
+samplerate_deg, data_deg = wavfile.read('degraded.wav')
+length_deg = data_deg.shape[0] / samplerate_deg
+time_deg = np.linspace(0., length_deg, data_deg.shape[0])
+plt.subplot(3, 1, 2)
+plt.plot(time_deg,data_deg)
+plt.subplots_adjust(hspace=0.5)
+plt.title("Degraded Signal")
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude") 
 
 # Load .mat file
 degraded_files = loadmat('degraded_points.mat')
 
 # Extract the keys of the dictionary
 print(degraded_files.keys())
+
+# Extract the array from the dictionary using the key
 x = degraded_files["degraded_point"]
 
 # Extract the click location
 clicks = np.where(x == 1)
-points = clicks[0]
-P = len(points)
-#print(P)
-#print(points)
 
+#Extract array from tuple
+click_index = clicks[0]
+
+#Number of clicks
+num_clicks = len(click_index)
 
 # Window Size
 win_len = int(input('Enter the window size : '))
+#win_len = x = range(1, 50, 2)
 
+#Check wheather the filter window size is ODD or EVEN
 if win_len % 2 == 1:
     print('The window size is ODD')
 else:
@@ -48,47 +62,51 @@ else:
 
 
 
-def median_filter(data, points, P, win_len):
-    data_new = data
-    for i in range(P):
-        A = int((win_len - 1) / 2)
-        inputVar = data_new[points[i] - A : points[i] + A + 1]
-        #print(inputVar)
+def median_filter(data_deg, click_index, num_clicks, win_len):
+    '''
+    
+    '''
+    data_new = data_deg
+    for i in range(num_clicks):
+        #Number of padded zeros required
+        pad_num = int((win_len - 1) / 2)
+
+        #Extract data around clicks
+        inputVar = data_new[click_index[i] - pad_num : click_index[i] + pad_num + 1]
         N = len(inputVar)
-        padded_input = np.pad(inputVar, (A, A), 'constant', constant_values=(0, 0))
-        #print(padded_input)
-        input_len = len(padded_input)
-        c = np.zeros(N)
+
+        #Add padded zeros
+        padded_input = np.pad(inputVar, (pad_num, pad_num), 'constant', constant_values=(0, 0))
+        
+        new_mat = np.zeros(N)
         for j in range(N):
             a = padded_input[j:win_len+j]
             b = np.sort(a)
-            c[j] = b[int((win_len - 1) / 2)]
-        #print(c)
-        data_new[points[i] - A : points[i] + A + 1] = c
+            new_mat[j] = b[int((win_len - 1) / 2)]
+        data_new[click_index[i] - pad_num : click_index[i] + pad_num + 1] = new_mat
     return data_new
 
+start_time = datetime.now()
 for i in tqdm(range(100)):
-    clean_data = median_filter(data, points, P, win_len)
+    restored_data = median_filter(data_deg, click_index, num_clicks, win_len)
     sleep(0.3)
-wavfile.write("restored.wav", samplerate, clean_data)
-
-
-length = clean_data.shape[0] / samplerate
-print(f"length = {length}s")
-time = np.linspace(0., length, clean_data.shape[0])
-plt.figure(figsize=(15, 5))
-plt.plot(time, clean_data, label="Restored Signal")
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time))
+wavfile.write("restored.wav", samplerate_org, restored_data)
+length = restored_data.shape[0] / samplerate_org
+time_restored = np.linspace(0., length, restored_data.shape[0])
+plt.subplot(3, 1, 3)
+plt.plot(time_restored,restored_data)
+plt.subplots_adjust(hspace=0.5)
+plt.title("Restored Signal using Median Filter ")
 plt.xlabel("Time [s]")
 plt.ylabel("Amplitude")
-plt.show()   
+plt.show()  
+
+MSE = mean_squared_error(data_org, restored_data)
+print(MSE)
 
 
 
-samplerate2, orginal_data = wavfile.read('clean_input.wav')
-length = orginal_data.shape[0] / samplerate2
-print(f"length = {length}s")
 
-# MSE = mean_squared_error(orginal_data, clean_data)
-# print(MSE)
 
-    
